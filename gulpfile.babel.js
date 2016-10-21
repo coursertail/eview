725b04503e52577417ede805c0af681d1c20b931
coursertail <l00234726@CHINA.HUAWEI.COM>
2016-10-22 00:55:00 +0800

    env: add $paths

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,14 @@
 import gulp from 'gulp';
 import clean from 'gulp-clean';
 
+const paths = {
+  build: 'build/',
+  src: 'src/',
+  dist: 'dist/'
+};
+
 gulp.task('clean', function(){
   return gulp.src([
-    'dist','!package.json'
+    `${paths.dist}`,'!package.json'
   ]).pipe(clean());
 });
